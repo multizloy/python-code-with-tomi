77d8dd581c75eeba3d4f24b732029b5261747ee4
Renat Shabanov <multizloy@gmail.com>
2024-07-13 09:33:06 +0300

    создали модель поста

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    createdAt = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
